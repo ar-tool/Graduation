433a42e6d06a2ac11cdc636a720de9e62ebd2bc6
yichang <284277590@qq.com>
2017-04-23 23:53:26 +0800

    commit my

--- Graduation/app/src/main/java/com/oujian/graduation/fragment/MyFragment.java
+++ Graduation/app/src/main/java/com/oujian/graduation/fragment/MyFragment.java
@@ -1,23 +1,31 @@
 package com.oujian.graduation.fragment;
 
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.oujian.graduation.R;
+import com.oujian.graduation.activity.LoginActivity;
 import com.oujian.graduation.base.BaseFragment;
+import com.oujian.graduation.common.MyContext;
+import com.oujian.graduation.view.ExitDialog;
+
+import butterknife.Bind;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MyFragment extends BaseFragment {
 
-
     private View mRootView;
-
+    @Bind(R.id.my_rl_exit)
+    RelativeLayout mExit;
     public MyFragment() {
         // Required empty public constructor
     }
@@ -40,11 +48,36 @@ public class MyFragment extends BaseFragment {
 
     @Override
     protected void initListeners() {
-
+        mExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getExitDialog();
+            }
+        });
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    private void getExitDialog(){
+        ExitDialog.Builder builder = new ExitDialog.Builder(getActivity());
+        builder.setPositiveButton(new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                //设置你的操作事项
+                MyContext.getInstance().resetUserInfo();//重置个人信息
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton(new android.content.DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
     }
 }
